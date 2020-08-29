8ca2687e26651829e500c1c9c3d5b17b13d96da3
David Fong <vkelim@bigpond.com>
2020-08-29 17:20:24 +1000

    CV connected to webpage

--- R/build.R
+++ R/build.R
@@ -1,1 +1,13 @@
 blogdown::build_dir('static')
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("static/CV/index.Rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(
+  input = tmp_html_cv_loc,
+  output = "static/CV/cv.pdf"
+)
